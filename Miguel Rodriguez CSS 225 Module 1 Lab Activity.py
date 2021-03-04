0993481dd54050dd22cb3abbea42bcbd8c1fd4d4
miguelr22 <72761857+miguelr22@users.noreply.github.com>
2021-03-04 11:47:22 -0600

    Add files via upload

---
+++ Miguel Rodriguez CSS 225 Module 1 Lab Activity.py
@@ -0,0 +1,39 @@
+
+#Miguel Rodriguez
+#January 16,2021
+#CSS 225
+#Module 1 Lab Activity
+#Worth 10 points each:
+
+#This line evaluates to 4 because adding 2 and 2 gives you 4.
+2+2
+
+#This line evaluates to 3 because dividing 12 and 4 gives you 3.
+12/4
+
+#This lines evaluates to 0 because 8 moduled by 2 gives you 9.
+8%2
+
+#This lines evaluates to 696 because multiplying 100 by 7 gives you 700 and subtracting that by 6 gives you 696.
+100*7-4
+
+#This lines evaluates to 300 because multiplying 100 by the parentheses 7 and 4 gives you 300.
+100*(7-4)
+
+#This lines evaluates to 6.6 because 80 moduled by the parentheses 8 and 9 gives you 13.6 and subtracting that by 7 gives you 6.6.
+80%(8+9)-7
+
+#This line evaluates to true because 10 is greater than 7.
+10 > 7
+
+#This lines evaluates to false because 20 is greater than not equal to 20.
+20 <= 20
+
+#This line evaluates to 10 because 20 adding 10 is 30 and 10 is not equal to 7.  
+20 != 30 and 10 == 7
+
+#This line evaluates to equal because 100 is equal to 100 and 7 moduled by 2 gives you 100 and that equals to 1.
+100 > 98+2 or 7%2 == 1
+
+#BONUS: Modify this code to display the result of each line to the user through the console when this file is run
+#       (there are several ways to do this).
